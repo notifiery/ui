05f5c71cf7977e2611ec6f08be11ec8af470b2eb
notifiery <notifiery@gmail.com>
2021-06-08 23:53:27 +0500

    Update App.tsx

--- colors/src/App.tsx
+++ colors/src/App.tsx
@@ -116,26 +116,38 @@ function App() {
   // ]);
 
   function calculateChroma(x: number, chroma1: number, chroma2: number): number {
-    return chromaInterpolator(x);
+    return 0.25; //chromaInterpolator(x);
 
     return 0.25 /*Math.max(0.25, 1
       - (chroma1 * x)
       - (chroma2 * (x ** 2)));*/
   }
 
-  function calculateLightness(x: number, lightness1: number, lightness2: number, lightness3: number): number {
-    // return Math.max(0, 1
-    //   - (lightness1 * x)
-    //   - (lightness2 * (x ** 2))
-    //   - (lightness3 * (x ** 3)));
+  function calculateLightness(x: number, lightness1: number, lightness2: number, lightness3: number, lightness4: number, lightness5: number): number {
+    return Math.max(0, 1
+      - (lightness1 * (x ** (1/2)))
+      - (lightness2 * (x ** (1/3)))
+      - (lightness3 * x)
+      - (lightness4 * (x ** 2))
+      - (lightness5 * (x ** 3))
+    );
 
-    const lightnessInterpolator = interpolator([
-      [0, 1],
-      [lightness3, lightness1],
-      [1, lightness2]
-    ]);
+    // console.log(
+    //   lightness1,
+    //   lightness2,
+    //   lightness3,
+    //   lightness4,
+    //   lightness5
+    // );
 
-    return lightnessInterpolator(x);
+    // const lightnessInterpolator = interpolator([
+    //   [0, 1],
+    //   [lightness3, lightness1],
+    //   [lightness4, lightness5],
+    //   [1, lightness2]
+    // ]);
+
+    // return lightnessInterpolator(x);
   }
 
   function buildPalette({
@@ -145,6 +157,8 @@ function App() {
     lightness1,
     lightness2,
     lightness3,
+    lightness4,
+    lightness5,
     chroma1,
     chroma2
   }: {
@@ -154,6 +168,8 @@ function App() {
     lightness1: number,
     lightness2: number,
     lightness3: number,
+    lightness4: number,
+    lightness5: number,
     chroma1: number,
     chroma2: number
   }) {
@@ -164,7 +180,7 @@ function App() {
 
     for (let stepIndex = 0; stepIndex < totalSteps; stepIndex++) {
       const stepPercentage = stepIndex / totalSteps;
-      const lightness = calculateLightness(stepPercentage, lightness1, lightness2, lightness3);
+      const lightness = calculateLightness(stepPercentage, lightness1, lightness2, lightness3, lightness4, lightness5);
 
       const color = culori.oklab({
         l: lightness,
@@ -193,7 +209,7 @@ function App() {
         const C = chroma * stepPercentage;
         const a = C * Math.cos(h);
         const b = C * Math.sin(h);
-        const lightness = calculateLightness(stepPercentage, lightness1, lightness2, lightness3);
+        const lightness = calculateLightness(stepPercentage, lightness1, lightness2, lightness3, lightness4, lightness5);
 
         const color = culori.oklab({
           l: lightness,
@@ -228,17 +244,23 @@ function App() {
     return hues;
   }
 
-  function getMinLightContrast(palette: any, stepIndex: number) {
-    return palette.reduce((contrast: number, steps: any) => {
-      return Math.min(contrast, steps[stepIndex].lightContrast);
-    }, Infinity);
+  function getLightLoss(palette: any, stepIndex: number, targetContrast: number) {
+    return palette.reduce((loss: number, steps: any) => {
+      return loss + Math.abs(steps[stepIndex].lightContrast - targetContrast);
+    }, 0);
   }
 
-  function getMaxLightContrast(palette: any, stepIndex: number) {
-    return palette.reduce((contrast: number, steps: any) => {
-      return Math.max(contrast, steps[stepIndex].lightContrast);
-    }, -Infinity);
-  }
+  // function getMinLightContrast(palette: any, stepIndex: number) {
+  //   return palette.reduce((contrast: number, steps: any) => {
+  //     return Math.min(contrast, steps[stepIndex].lightContrast);
+  //   }, Infinity);
+  // }
+
+  // function getMaxLightContrast(palette: any, stepIndex: number) {
+  //   return palette.reduce((contrast: number, steps: any) => {
+  //     return Math.max(contrast, steps[stepIndex].lightContrast);
+  //   }, -Infinity);
+  // }
 
   function getMinDarkContrast(palette: any, stepIndex: number) {
     return palette.reduce((contrast: number, steps: any) => {
@@ -274,7 +296,9 @@ function App() {
     // for (let ho = 0; ho <= 5; ho += 0.1) {
     for (let l1 = 0; l1 <= 1; l1 += 0.1) {
       for (let l2 = 0; l2 <= 1; l2 += 0.1) {
-        for (let l3 = 0.1; l3 <= 0.9; l3 += 0.1) {
+        for (let l3 = 0; l3 <= 1; l3 += 0.1) {
+          for (let l4 = 0; l4 <= 1; l4 += 0.1) {
+            for (let l5 = 0; l5 <= 1; l5 += 0.1) {
         // for (let c1 = 0; c1 <= 1; c1 += 0.1) {
           // for (let c2 = 0; c2 <= 1; c2 += 0.1) {
             const lightPalette = buildPalette({
@@ -284,24 +308,35 @@ function App() {
               lightness1: l1,
               lightness2: l2,
               lightness3: l3,
+              lightness4: l4,
+              lightness5: l5,
               chroma1: 0,
               chroma2: 0
             });
 
-            const minLightContrast700 = getMinLightContrast(lightPalette, 7);
-            const maxLightContrast700 = getMaxLightContrast(lightPalette, 7);
-            const averageLightContrast700 = (minLightContrast700 + maxLightContrast700) / 2;
-            const targetLightContrast700 = 4.5;
+            const targetContrast200 = 1.1;
+            const targetContrast700 = 4.5;
+            const targetContrast800 = targetContrast700 * GOLDEN_RATIO;
+            const targetContrast900 = targetContrast800 * GOLDEN_RATIO;
 
-            const minLightContrast800 = getMinLightContrast(lightPalette, 8);
-            const maxLightContrast800 = getMaxLightContrast(lightPalette, 8);
-            const averageLightContrast800 = (minLightContrast800 + maxLightContrast800) / 2;
-            const targetLightContrast800 = /* 7 */ targetLightContrast700 * GOLDEN_RATIO;
+            const loss200 = getLightLoss(lightPalette, 2, targetContrast200);
+            const loss700 = getLightLoss(lightPalette, 7, targetContrast700);
+            const loss900 = getLightLoss(lightPalette, 9, targetContrast900);
 
-            const minLightContrast900 = getMinLightContrast(lightPalette, 9);
-            const maxLightContrast900 = getMaxLightContrast(lightPalette, 9);
-            const averageLightContrast900 = (minLightContrast900 + maxLightContrast900) / 2;
-            const targetLightContrast900 = /*11*/ /*10*/ targetLightContrast800 * GOLDEN_RATIO;
+            // const minLightContrast700 = getMinLightContrast(lightPalette, 7);
+            // const maxLightContrast700 = getMaxLightContrast(lightPalette, 7);
+            // const averageLightContrast700 = (minLightContrast700 + maxLightContrast700) / 2;
+            // const targetLightContrast700 = 4.5;
+
+            // const minLightContrast800 = getMinLightContrast(lightPalette, 8);
+            // const maxLightContrast800 = getMaxLightContrast(lightPalette, 8);
+            // const averageLightContrast800 = (minLightContrast800 + maxLightContrast800) / 2;
+            // const targetLightContrast800 = /* 7 */ targetLightContrast700 * GOLDEN_RATIO;
+
+            // const minLightContrast900 = getMinLightContrast(lightPalette, 9);
+            // const maxLightContrast900 = getMaxLightContrast(lightPalette, 9);
+            // const averageLightContrast900 = (minLightContrast900 + maxLightContrast900) / 2;
+            // const targetLightContrast900 = /*11*/ /*10*/ targetLightContrast800 * GOLDEN_RATIO;
 
             // const minLightContrast600 = getMinLightContrast(lightPalette, 6);
             // const maxLightContrast600 = getMaxLightContrast(lightPalette, 6);
@@ -333,9 +368,22 @@ function App() {
             // const averageLightContrast100 = (minLightContrast100 + maxLightContrast100) / 2;
             // const targetLightContrast100 = /* 7 */ targetLightContrast200 / GOLDEN_RATIO;
 
-            const lightLoss = Math.abs(averageLightContrast700 - targetLightContrast700)
-              + Math.abs(averageLightContrast800 - targetLightContrast800)
-              + Math.abs(averageLightContrast900 - targetLightContrast900)
+            // const lightLoss = 0
+            //   + Math.abs(minLightContrast700 - targetLightContrast700)
+            //   + Math.abs(maxLightContrast700 - targetLightContrast700)
+              // + Math.pow(minLightContrast800 - targetLightContrast800, 2)
+              // + Math.pow(maxLightContrast800 - targetLightContrast800, 2)
+              // + Math.pow(minLightContrast900 - targetLightContrast900, 2)
+              // + Math.pow(maxLightContrast900 - targetLightContrast900, 2)
+
+          const lightLoss = loss200 + loss700 + loss900;
+
+            // const lightLoss = Math.abs(minLightContrast700 - targetLightContrast700)
+            //   + Math.abs(maxLightContrast700 - targetLightContrast700)
+            //   // + Math.abs(minLightContrast800 - targetLightContrast800)
+            //   // + Math.abs(maxLightContrast800 - targetLightContrast800)
+            //   + Math.abs(minLightContrast900 - targetLightContrast900)
+            //   + Math.abs(maxLightContrast900 - targetLightContrast900)
               // + Math.abs(averageLightContrast600 - targetLightContrast600)
               // + Math.abs(averageLightContrast500 - targetLightContrast500)
               // + Math.abs(averageLightContrast400 - targetLightContrast400)
@@ -346,14 +394,20 @@ function App() {
             if (lightLoss < bestLightLoss) {
               bestLightPalette = lightPalette;
               bestLightLoss = lightLoss;
+
+              console.log('loss700', loss700);
+
+              // console.log('700', Math.abs(averageLightContrast700 - targetLightContrast700));
+              // console.log('800', Math.abs(averageLightContrast800 - targetLightContrast800));
+              // console.log('900', Math.abs(averageLightContrast900 - targetLightContrast900));
               // bestl1 = l1;
               // bestl2 = l2;
               // bestc1 = c1;
               // bestc2 = c2;
               // bestho = ho;
             }
-          // }
-        // }
+          }
+        }
           }
       }
     }
@@ -389,6 +443,8 @@ function App() {
               lightness1: l1,
               lightness2: l2,
               lightness3: 0,
+              lightness4: 0,
+              lightness5: 0,
               chroma1: c1,
               chroma2: c2
             });
